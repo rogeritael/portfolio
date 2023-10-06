b127083373c5b76f9e3dfd1fb1e73c32ca19643f
Roger Rosa <rogeritael.contato@gmail.com>
2023-10-06 14:53:08 -0300

    adicionando projetos

--- src/mocks/projects.ts
+++ src/mocks/projects.ts
@@ -1,6 +1,8 @@
 // import jjng from '../assets/projects/jjng.svg'
 // import pizzaria from '../assets/projects/pizza.svg'
 import quando_lanca from '../assets/projects/quando_lanca_mockup.png'
+import loopstudios from '../assets/projects/loopstudios.png'
+import skillet from '../assets/projects/skillet.png'
 
 export const projects = [
     // {
@@ -24,6 +26,24 @@ export const projects = [
     {
         cover: quando_lanca,
         project_name: 'Quando Lança',
+        description: 'Plataforma gamer que notifica os usuários sobre anúncios e lançamentos de jogos, além de permitir que os usuários marquem interesse em jogos para receber atualizações.',
+        technologies:  ['Next 13', 'TypeScript', 'Styled-Components', 'Hooks', 'Context API'],
+        content_side: 'left',
+        gitLink: 'https://github.com/rogeritael/Topolinos-Pizzaria',
+        prodLink: 'https://topolinos-pizzaria.vercel.app/',
+    },
+    {
+        cover: loopstudios,
+        project_name: 'Loop Studios Landing Page',
+        description: 'Plataforma gamer que notifica os usuários sobre anúncios e lançamentos de jogos, além de permitir que os usuários marquem interesse em jogos para receber atualizações.',
+        technologies:  ['Next 13', 'TypeScript', 'Styled-Components', 'Hooks', 'Context API'],
+        content_side: 'right',
+        gitLink: 'https://github.com/rogeritael/Topolinos-Pizzaria',
+        prodLink: 'https://topolinos-pizzaria.vercel.app/',
+    },
+    {
+        cover: skillet,
+        project_name: 'Skillet Landing Page',
         description: 'Plataforma gamer que notifica os usuários sobre anúncios e lançamentos de jogos, além de permitir que os usuários marquem interesse em jogos para receber atualizações.',
         technologies:  ['Next 13', 'TypeScript', 'Styled-Components', 'Hooks', 'Context API'],
         content_side: 'left',

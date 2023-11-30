1102d82a081b4702e91ed6bff80aa6f8c5907850
rogeritael <rogeritael.contato@gmail.com>
2023-11-30 13:33:49 -0300

    adicionando o link de automação ao menu mobile

--- src/components/lg/MobileMenu/index.tsx
+++ src/components/lg/MobileMenu/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { StyledSection } from "./styles";
 
 import { IoIosClose } from 'react-icons/io'
@@ -18,6 +19,7 @@ export function MobileMenu({ isMobileMenuOpen, setIsMobileMenuOpen }: MobileMenu
                 <a href="#skills" onClick={() => setIsMobileMenuOpen(false)}>.habilidades&#40; &#41;</a>
                 <a href="#projects" onClick={() => setIsMobileMenuOpen(false)}>.projetos&#40; &#41;</a>
                 <a href="#contact_form" onClick={() => setIsMobileMenuOpen(false)}>.contato&#40; &#41;</a>
+                <Link to="/automation" onClick={() => setIsMobileMenuOpen(false)}>.automação&#40; &#41;</Link>
             </nav>
         </StyledSection>
     )
